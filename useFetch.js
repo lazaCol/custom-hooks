4fe6d7663c5919b4a0b8f8eb6a4f7916bb289834
Luis Miguel Aza Caicedo <laza@Colsof.com.co>
2020-10-17 09:09:19 -0500

    hooks personalizados

---
+++ useFetch.js
@@ -0,0 +1,35 @@
+import { useEffect, useState, useRef } from "react";
+
+export const useFetch = (url) => {
+  const isMounted = useRef(true);
+
+  const [state, setstate] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    setstate({ data: null, loading: true, error: null });
+
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        if (isMounted.current) {
+          setstate({
+            error: null,
+            data: data,
+            loading: false,
+          });
+        }
+      });
+  }, [url]);
+
+  return state;
+};
